61ad771ed92766c28582dc8cbd5e6ca8d74fd89d
Shree Sriadibhatla <shreesriadibhatla@b01-aruba-authenticated-10-110-247-151.wlan.cpp.edu>
2022-03-14 13:56:31 -0700

    attempting to push implemented methods

--- src/main/java/LinkedStack.java
+++ src/main/java/LinkedStack.java
@@ -13,6 +13,66 @@ public final class LinkedStack<T> implements StackInterface<T>
         topNode = null;
     } // end default constructor
 
+    @Override
+    public void push(T newEntry) {
+        Node<T> newNode = new Node<T>(newEntry);
+
+        if (topNode == null) {
+            topNode = newNode;
+        }
+        else {
+            Node<T> temp = topNode;
+            topNode = newNode;
+            newNode.next = temp;
+        }
+        System.out.println(newEntry + " pushed to stack");
+    }
+
+    @Override
+    public T pop() {
+        T retValue = null;
+        if (topNode == null) {
+            throw new EmptyStackException();
+        }
+        else {
+            retValue = topNode.data;
+            topNode = topNode.next;
+        }
+        return retValue;
+    }
+
+    @Override
+    public T peek() {
+        if (topNode == null) {
+            System.out.println("Stack is empty");
+            throw new EmptyStackException();
+        }
+        else {
+            return topNode.data;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (topNode == null) {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    @Override
+    public void clear() {
+        if (topNode == null) {
+            System.out.println("Stack is empty");
+            throw new EmptyStackException();
+        }else {
+            while(topNode.next != null) {
+                topNode.data = null;
+            }
+        }
+    }
+
 //  < Implementations of the stack operations go here. >
 //  . . .
 
