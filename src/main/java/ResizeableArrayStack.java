a76eb612c426a9214b0c541245ed395aa63445ee
jafurman <jafbird52@gmail.com>
2022-03-14 17:18:22 -0700

    Finished evaluatePostfix and made changes/additions to other methods in ResizeableArrayStack

--- src/main/java/ResizeableArrayStack.java
+++ src/main/java/ResizeableArrayStack.java
@@ -4,59 +4,59 @@ import java.util.EmptyStackException;
 import java.util.*;
 
 public final class ResizeableArrayStack<T> implements StackInterface<T>
+{
+    private T[] stack; //Array of stack entries we will be using
+    private int topIndex; //top of the stack is measured with this integer
+    private boolean integrityOK = false;
+    private static final int DEFAULT_CAPACITY = 50;
+    private static final int MAX_CAPACITY = 10000;  //stack cannot be greater than this integer
+
+    public ResizeableArrayStack()
     {
-        private T[] stack; //Array of stack entries we will be using
-        private int topIndex; //top of the the stack is measured with this integer
-        private boolean integrityOK = false;
-        private static final int DEFAULT_CAPACITY = 50;
-        private static final int MAX_CAPACITY = 10000;  //stack cannot be greater than this integer
+        this(DEFAULT_CAPACITY);
+    } //end default constructor
 
-public ResizeableArrayStack()
-        {
-            this(DEFAULT_CAPACITY);
-        } //end default constructor
-
-public ResizeableArrayStack( int initialCapacity )
-        {
-            integrityOK = false; //check integrity
-            checkCapacity(initialCapacity); //sanitize user data
-            // The cast is safe because the new array contains null entries
-            @SuppressWarnings("unchecked")
-            T[] tempStack = (T[])new Object[initialCapacity];
-            stack = tempStack;
-            topIndex = -1;
-            integrityOK = true;
-        } // end constructor
+    public ResizeableArrayStack( int initialCapacity )
+    {
+        integrityOK = false; //check integrity
+        checkCapacity(initialCapacity); //sanitize user data
+        // The cast is safe because the new array contains null entries
+        @SuppressWarnings("unchecked")
+        T[] tempStack = (T[])new Object[initialCapacity];
+        stack = tempStack;
+        topIndex = -1;
+        integrityOK = true;
+    } // end constructor
 
     @Override
-public void push(T newEntry)
-        {
-            checkIntegrity(); //taken from notes, pushes a new entry to the top of the stack
-            ensureCapacity();
-            stack[topIndex + 1] = newEntry;
-            topIndex++; //then adds one to the top counter so that the peek method works
-        } //end push
+    public void push(T newEntry)
+    {
+        checkIntegrity(); //taken from notes, pushes a new entry to the top of the stack
+        ensureCapacity();
+        stack[topIndex + 1] = newEntry;
+        topIndex++; //then adds one to the top counter so that the peek method works
+    } //end push
 
     @Override
-public T pop()
+    public T pop()
+    {
+        checkIntegrity();
+        if (isEmpty())
         {
-            checkIntegrity();
-            if (isEmpty())
-            {
-                throw new EmptyStackException();
-            } else
-            {
-                T top = stack[topIndex];
-                stack[topIndex] = null;
-                topIndex--;
-                return top;
-            } // end if
+            throw new EmptyStackException();
+        } else
+        {
+            T top = stack[topIndex];
+            stack[topIndex] = null;
+            topIndex--;
+            return top;
+        } // end if
 
-        } //end pop
+    } //end pop
 
     @Override
-public T peek()
-        {
+    public T peek()
+    {
         checkIntegrity();
         if (isEmpty())
         {
@@ -65,71 +65,88 @@ public T peek()
         {
             return stack[topIndex]; //will return whatever is at the top of the stack
         }
-        } //end peek
+    } //end peek
 
     @Override
-public boolean isEmpty()
-        {
+    public boolean isEmpty()
+    {
         return topIndex < 0;
-        }
+    }
 
     @Override
-public void clear()
-        {
+    public void clear()
+    {
         checkIntegrity();
         while (topIndex > -1)
-            {
-                stack[topIndex] = null;
-                topIndex--;
-            } // end while
-            //Assertion: topIndex is -1
-        } //end clear
+        {
+            stack[topIndex] = null;
+            topIndex--;
+        } // end while
+        //Assertion: topIndex is -1
+    } //end clear
+    public ResizeableArrayStack<Integer> evaluatePostfix(String exp)
+    {
+        ResizeableArrayStack<Integer> valueStack = new ResizeableArrayStack<Integer>();    // Create postfix stack
+        int n = exp.length();
 
-/*
-public T evaluatePostfix(postfix)
+        for(int i=0;i<n;i++)
         {
-// Evaluates a postfix expression.
-        T[] valueStack = (T[])new Object[];
-        while (!valueStack.isEmpty())
-        {
-        nextCharacter = next nonblank character of postfix
-        switch (nextCharacter)
-        {
-            case variable:
-                valueStack.push(value of the variable nextCharacter) break
-            case '+' : case '-' : case '*' : case '/' : case '^' :
-            operandTwo = valueStack.pop()
-            operandOne = valueStack.pop()
-            result = the result of the operation in nextCharacter and its operands
-            operandOne and operandTwo valueStack.push(result)
-            break
-            default: break // Ignore unexpected characters
-        } }
-return valueStack.peek()
-        } //end evaluatePostfix
- */
-
-private void checkCapacity(int capacity)
-        {
-            if (capacity > MAX_CAPACITY)
-                throw new IllegalStateException("Attempt to create a bag whose " +
-                        "capacity exceeds allowed " +
-                        "maximum of " + MAX_CAPACITY);
-        } // end checkCapacity
-private void checkIntegrity()
-        {
-            if (!integrityOK)   //Method to check integrity and use for every other method. Means of sanitizing data.
+            if(isOperator(exp.charAt(i)))
             {
-                throw new SecurityException("Uninitialized object used to call an arrayBag method");
+                // pop top 2 operands.
+                int operatorOne =  valueStack.pop();
+                int operatorTwo = valueStack.pop();
+
+                // evaluate in reverse order i.e. op2 operator op1.
+                switch (exp.charAt(i)) {
+                    case '+' -> valueStack.push(operatorTwo + operatorOne);
+                    case '-' -> valueStack.push(operatorTwo - operatorOne);
+                    case '*' -> valueStack.push(operatorTwo * operatorOne);
+                    case '/' -> valueStack.push(operatorTwo / operatorOne);
+                }
+
             }
-        }  //end checkInitialization
-private void ensureCapacity()
-        {
-            if (topIndex >= stack.length - 1) // If array is full, double its size
+            // Current Char is Operand simple push into stack
+            else
             {
-                int newLength = 2 * stack.length;
-                checkCapacity(newLength);
-                stack = Arrays.copyOf(stack, newLength);
-            } // end if
+                // convert to integer
+                int result = exp.charAt(i) - '0';
+                valueStack.push(result);
+            }
         }
+
+        // Stack at End will contain result.
+        System.out.println(valueStack.pop());
+        return valueStack;
+    } //end evaluatePostfix
+    private void checkCapacity(int capacity)
+    {
+        if (capacity > MAX_CAPACITY)
+            throw new IllegalStateException("Attempt to create a bag whose " +
+                    "capacity exceeds allowed " +
+                    "maximum of " + MAX_CAPACITY);
+    } // end checkCapacity
+    private void checkIntegrity()
+    {
+        if (!integrityOK)   //Method to check integrity and use for every other method. Means of sanitizing data.
+        {
+            throw new SecurityException("Uninitialized object used to call an arrayBag method");
+        }
+    }  //end checkInitialization
+    private void ensureCapacity()
+    {
+        if (topIndex >= stack.length - 1) // If array is full, double its size
+        {
+            int newLength = 2 * stack.length;
+            checkCapacity(newLength);
+            stack = Arrays.copyOf(stack, newLength);
+        } // end if
+    }
+    public boolean isOperator(char ch) //this method will be used in evaluatePostfix to check and make sure what it being used in the stack is correct
+    {
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            return true;
+        }
+        return false;
+    }
 } //end ResizeableArrayStack
