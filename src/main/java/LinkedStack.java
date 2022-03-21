03a9cc180d8cb945d04ad57727740ad80690761e
Shree Sriadibhatla <shreesriadibhatla@Shrees-MacBook-Air.local>
2022-03-21 00:59:47 -0700

    Added postfix expression

--- src/main/java/LinkedStack.java
+++ src/main/java/LinkedStack.java
@@ -65,7 +65,73 @@ public final class LinkedStack<T> implements StackInterface<T> {
 	}
 
 //  < Implementations of the stack operations go here. >
-//  . . .
+  public LinkedStack<Integer> evaluatePostfix(String exp)
+    {
+        LinkedStack<Integer> valueStack = new LinkedStack<Integer>();    // Create postfix stack
+        int n = exp.length();
+
+        for(int i=0;i<n;i++)
+        {
+            if(isOperator(exp.charAt(i)))
+            {
+                // pop top 2 operands.
+                int operatorOne =  valueStack.pop();
+                int operatorTwo = valueStack.pop();
+
+                // evaluate in reverse order i.e. op2 operator op1.
+                switch (exp.charAt(i)) {
+                    case '+' -> valueStack.push(operatorTwo + operatorOne);
+                    case '-' -> valueStack.push(operatorTwo - operatorOne);
+                    case '*' -> valueStack.push(operatorTwo * operatorOne);
+                    case '/' -> valueStack.push(operatorTwo / operatorOne);
+                }
+
+            }
+            // Current Char is Operand simple push into stack
+            else
+            {
+                // convert to integer
+                int result = exp.charAt(i) - '0';
+                valueStack.push(result);
+            }
+        }
+
+        // Stack at End will contain result.
+        System.out.println(valueStack.pop());
+        return valueStack;
+        //correction2
+        //return valueStack.pop();
+    } //end evaluatePostfix
+    private void checkCapacity(int capacity)
+    {
+        if (capacity > MAX_CAPACITY)
+            throw new IllegalStateException("Attempt to create a bag whose " +
+                    "capacity exceeds allowed " +
+                    "maximum of " + MAX_CAPACITY);
+    } // end checkCapacity
+    private void checkIntegrity()
+    {
+        if (!integrityOK)   //Method to check integrity and use for every other method. Means of sanitizing data.
+        {
+            throw new SecurityException("Uninitialized object used to call an arrayBag method");
+        }
+    }  //end checkInitialization
+    private void ensureCapacity()
+    {
+        if (topIndex >= stack.length - 1) // If array is full, double its size
+        {
+            int newLength = 2 * stack.length;
+            checkCapacity(newLength);
+            stack = Arrays.copyOf(stack, newLength);
+        } // end if
+    }
+    public boolean isOperator(char ch) //this method will be used in evaluatePostfix to check and make sure what it being used in the stack is correct
+    {
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            return true;
+        }
+        return false;
+    }
 
 	private class Node<T> {
 		private T data; // Entry in stack
